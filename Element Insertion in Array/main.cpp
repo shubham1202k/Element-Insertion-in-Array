fef9263306a1fe3e66af17e4dacc987042452641
shubham1202k <shubham1202k@gmail.com>
2021-01-19 17:46:01 +0530

    Update main.cpp

--- Element Insertion in Array/main.cpp
+++ Element Insertion in Array/main.cpp
@@ -1,14 +1,35 @@
-//
-//  main.cpp
-//  Element Insertion in Array
-//
-//  Created by shubham sharma on 19/01/21.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+void display(int arr[], int n){
+    // Code for displaying elements of an array
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+
+int indInsertion(int arr[], int size, int element, int capacity, int index){
+    // code for Insertion in an array at a given index
+    if(size>=capacity){
+        return -1;
+    }
+    for (int i = size-1; i >=index; i--)
+    {
+        arr[i+1] = arr[i];
+    }
+    arr[index] = element;
+    return 1;
+}
+
+int main(){
+    int arr[10] = {4, 9, 14, 47, 90};
+    int size = 5, element = 45, index=1;
+    display(arr, size);
+    indInsertion(arr, size, element, 100, index);
+    size +=1;
+    display(arr, size);
     return 0;
 }
